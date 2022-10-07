8d03b78bd7d838296c499b1fcceb01bfd35c918b
kimberlyydanij <kimberlydanij@gmail.com>
2022-10-07 10:34:06 +0900

    20221007 | Add

--- src/java003_statements/Java031_for.java
+++ src/java003_statements/Java031_for.java
@@ -1,25 +1,16 @@
 package java003_statements;
 
-/*
- * 1  2  3  4 
- * 5  6  7  8
- * 9 10 11 12
- */
 public class Java031_for {
 
 	public static void main(String[] args) {
-
-			int data = 0;
-			label :
 				
-		for(int row=1; row<=3; row++) {
-				for(int col =1; col<=4; col++) {
+		for(int row=1; row<=4; row++) {
+				for(int col =1; col<=5; col++) {
 				//data++;
-				System.out.printf("%4d",++data);
+				System.out.printf("%4d",col);
 				
-				if(data%4==0)
-					System.out.println("");
-					continue label;
+				if(col%5==0)
+					System.out.println("");					
 			}
 			
 		}
---
+++ src/java004_array/Java037_array.java
@@ -0,0 +1,10 @@
+package java004_array;
+
+public class Java037_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
