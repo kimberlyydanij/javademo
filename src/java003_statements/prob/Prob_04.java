1dbfcfdc4a75f5e2702eb3d1697ae2a532f68113
kimberlyydanij <kimberlydanij@gmail.com>
2022-10-09 22:50:50 +0900

    20221009 | Update

--- src/java003_statements/prob/Prob_04.java
+++ src/java003_statements/prob/Prob_04.java
@@ -12,6 +12,13 @@ public class Prob_04 {
 
 	public static void main(String[] args) {
 		
+		for(int x=0;x<=10;x++) {
+			for(int y=0;y<=10;y++) {
+				if((2*x)+(3*y)==10) {
+					System.out.printf("x=%d, y=%d \n",x,y);
+				} //end if
+			}  //end y
+		} //end x
 
 	}// end main()
 
