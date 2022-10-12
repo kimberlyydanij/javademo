9f9c41b4203f7b0b28bd689c03320924c581c0aa
kimberlyydanij <kimberlydanij@gmail.com>
2022-10-12 12:35:56 +0900

    20221012 | Add

---
+++ src/java006_class/part03/Java067_class.java
@@ -0,0 +1,17 @@
+package java006_class.part03;
+
+public class Java067_class {
+
+	public static void main(String[] args) {
+			
+		Rect rt = new Rect();
+		rt.width = 5;
+		rt.height = 3;
+		rt.color = "레드";
+		
+		rt.display();
+		
+
+	} // end main()
+
+} // end class
