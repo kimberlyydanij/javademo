b9b5f34d0984120ca586e7f85d33d04d6d604f88
kimberlyydanij <kimberlydanij@gmail.com>
2022-10-12 11:32:44 +0900

    20221012 | aDD

---
+++ src/java006_class/part01/Person.java
@@ -0,0 +1,15 @@
+package java006_class.part01;
+
+public class Person {
+	String name;
+	int age;
+	char gender;
+	
+	public void develop() {
+		System.out.println("개발한다");
+	} //develop
+	
+	public void run() {
+		System.out.println("달린다");
+	} //run
+} // class
