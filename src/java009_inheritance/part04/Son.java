60af401bf28e9cba1ea92d076427a346cf49b86b
kimberlyydanij <kimberlydanij@gmail.com>
2022-10-14 12:20:58 +0900

    20221014 | Add

--- src/java009_inheritance/part04/Son.java
+++ src/java009_inheritance/part04/Son.java
@@ -1,5 +1,19 @@
 package java009_inheritance.part04;
 
-public class Son {
+public class Son extends Parent {
+	String dept;
+	
+	public Son() {
+		
+	}
+	
+	public Son(String name, int age, String dept) {
+		super(name, age);
+		this.dept = dept;
+	}
 
+	public String toString() {
+
+		return String.format("%s %d %s", name, age, dept);
+	}
 }
---
+++ src/java009_inheritance/part05/Java096_inheritance.java
@@ -0,0 +1,8 @@
+package java009_inheritance.part05;
+
+public class Java096_inheritance {
+	public static void main(String[] args) {
+		
+	} // end main
+
+}
