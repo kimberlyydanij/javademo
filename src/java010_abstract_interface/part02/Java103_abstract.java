aeece326edfdbea24d5e26f38eec245bbd9dcbff
kimberlyydanij <kimberlydanij@gmail.com>
2022-10-17 18:01:40 +0900

    20221017 | Add

--- src/java010_abstract_interface/part02/Java103_abstract.java
+++ src/java010_abstract_interface/part02/Java103_abstract.java
@@ -6,7 +6,7 @@ public class Java103_abstract {
 		Rect rect = new Rect(10, 20);
 		System.out.println("사각형 넓이:" + rect.getArea());	
 		
-		Tri tri = new Tri(10, 20);
+		Tri tri = new Tri(10, 20); 
 		System.out.println("삼각형 넓이:" + tri.getArea());
 
 	}
