d2dd5871f2b5a6c383db6c4c071b3d12d1436c34
kimberlyydanij <kimberlydanij@gmail.com>
2022-10-19 12:48:38 +0900

    20221019 | Add

---
+++ src/java013_api/Java138_Random.java
@@ -0,0 +1,13 @@
+package java013_api;
+
+import java.util.Random;
+
+public class Java138_Random {
+
+	public static void main(String[] args) {
+		Random ran = new Random();
+		
+		
+	} //end main
+
+} //end class
