fddddfed33e0e22acf03bbbd93c54295101b53ae
kimberlyydanij <kimberlydanij@gmail.com>
2022-10-24 16:57:47 +0900

    20221024 | Add

--- src/java018_collection/Ascending.java
+++ src/java018_collection/Ascending.java
@@ -7,7 +7,8 @@ public class Ascending implements Comparator<Integer>{
 	@Override
 	public int compare(Integer o1, Integer o2) {
 		
-		return 0;
+		//return o2>o1 ? -1 : o2<o1 ? 1 : 0;
+		return o1.compareTo(o2);
 	}
 
 }
