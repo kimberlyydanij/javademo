f829a96ee991099f860c96011a6a439b0995b0ca
kimberlyydanij <kimberlydanij@gmail.com>
2022-10-24 11:18:31 +0900

    20221024 | Add

---
+++ src/java018_collection/Java188_LinkedList.java
@@ -0,0 +1,17 @@
+package java018_collection;
+
+/*
+ * 1. 배열 구조의 데이터 관리 : Vector, ArrayList
+ *                         ArrayList-추가-(순차적으로 처리할 때 사용)
+ * 2. 노드로 데이터 관리 : LinkedList
+ * 			           삽입, 삭제-(비순차적으로 처리할 때 사용)
+ * 3. List를 구현해 놓은 컬렉션
+ * 	  Vector, ArrayList, LinkedList는 add()해 준 순서대로 데이터를 관리 해준다.
+ */
+public class Java188_LinkedList {
+
+	
+
+	} //end main
+
+} //end class
