377770e85362f8d07fbc5f640bf91bb57554cb1c
KimberlyDaniJ <kimberlydanij@gmail.com>
2022-11-16 11:43:38 +0900

    20221116 | Update

--- java002_statements/Java018_if.java
+++ java002_statements/Java018_if.java
@@ -13,21 +13,18 @@ package java002_statements;
 public class Java018_if {
 
 	public static void main(String[] args) {
-		boolean member = false-; //회원(true) or 비회원(false)
+		boolean member = false; //회원(true) or 비회원(false)
 		String grade = "일반"; //회원등급(VIP,일반
 		
 		if(!member) {
 			//회원				
-			if(grade=="VIP") {
-				System.out.printf("고객님은 %s 이며 %d% 적립\n",grade,30);
-			}else {
-				System.out.printf("고객님은 %s 이며 %d% 적립\n",grade,10);
-			}
+			if(grade=="VIP") 
+				System.out.printf("고객님은 %s 이며 %d%% 적립\n",grade,30);
+			else if (grade=="일반")
+				System.out.printf("고객님은 %s 이며 %d%% 적립\n",grade,10);
 			//비회원
-		}else {
-			System.out.printf("고객님은 %s 이며 %d% 적립\n",grade, 0);
-						
-		}
+			else System.out.printf("고객님은 %s 이며 %d%% 적립\n",grade, 0);	
+		} // end if
 
 	}//end main()
 
